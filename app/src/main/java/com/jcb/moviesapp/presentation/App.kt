ab4de8245dbbd337955624432ed1dff5c0c077c7
Jockzan <jockzancardona@gmail.com>
2024-12-19 13:21:04 -0600

    arch modules data, domain, presentation, etc

---
+++ app/src/main/java/com/jcb/moviesapp/presentation/App.kt
@@ -0,0 +1,7 @@
+package com.jcb.moviesapp.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
---
+++ app/src/main/java/com/jcb/moviesapp/presentation/ui/movies/MoviesActivity.kt
@@ -0,0 +1,19 @@
+package com.jcb.moviesapp.presentation.ui.movies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MoviesActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Text("Hello Android")
+        }
+    }
+}
